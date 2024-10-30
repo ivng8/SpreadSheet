4413800d9fecc35c7dccc8ad9ec577a7f13c592f
Nathan Huang <longhaoh1370@gmail.com>
2024-10-29 20:33:31 -0400

    sets up jest testing

--- implementation/src/expressions/test/ExpressionBuilder.spec.ts
+++ implementation/src/expressions/test/ExpressionBuilder.spec.ts
@@ -3,7 +3,8 @@ import { IExpression } from "../../interfaces/IExpression";
 describe('ExpressionBuilder', (): void =>{
     let expression : IExpression;
 
-    it('', (): void => {
-
+    it('example test should pass', (): void => {
+        const value = 1 + 1;
+        expect(value).toBe(2);
     });
 });
