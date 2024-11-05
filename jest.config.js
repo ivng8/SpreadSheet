6305190329ef919cd7111ff40707fa50fc30e1d6
Nathan Huang <longhaoh1370@gmail.com>
2024-11-05 12:31:07 -0500

    Updated Jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/implementation/src'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json'
+      }
+    ]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  verbose: true
+};
