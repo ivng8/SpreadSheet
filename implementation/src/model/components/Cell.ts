7c1eb4a8c068372ad3ddb257ac8691cf1135d9c8
Ivan Ng <147109497+ivng8@users.noreply.github.com>
2024-11-07 23:52:59 -0500

    Update Cell.ts

--- implementation/src/model/components/Cell.ts
+++ implementation/src/model/components/Cell.ts
@@ -1,5 +1,5 @@
 import { IExpression } from '../interfaces/IExpression';
-// import { VersionHistory } from "./version/VersionHistory";
+import { VersionHistory } from "./version/VersionHistory";
 // import { EmptyExpression } from "./expressions/EmptyExpression";
 import { Director } from '../Director';
 import { SpreadSheet } from './SpreadSheet';
@@ -9,7 +9,7 @@ export class Cell {
   private address: string;
   private input: string;
   private expression: IExpression;
-  //private versionHistory: VersionHistory;
+  private versionHistory: VersionHistory;
   private sheet: SpreadSheet;
 
   public constructor(address: string, input: string, reference: SpreadSheet) {
@@ -30,6 +30,7 @@ export class Cell {
   public updateContents(newText: string): void {
     this.input = newText;
     this.expression = new Director().makeExpression(this.input, this.sheet, this);
+    this.versionHistory.addEntry(new VersionEntry());
   }
 
   public catchErrors(error: IError): void {
