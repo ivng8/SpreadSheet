15dd5edd9e7af1f4e8ad7c58212750fb51080de6
Ivan Ng <ivanng2121@gmail.com>
2024-11-20 10:37:03 -0500

    commented out use of emitters and replaced events with callback function

--- implementation/src/model/conflicts/MergeConflictResolver.ts
+++ implementation/src/model/conflicts/MergeConflictResolver.ts
@@ -1,7 +1,8 @@
-import { EventEmitter } from "stream";
+// import { EventEmitter } from "stream";
 import { MergeConflict } from "./MergeConflict";
 import { Cell } from "model/components/Cell";
 
+/** 
 export class MergeConflictResolver extends EventEmitter {
     private conflicts: MergeConflict[];
 
@@ -28,4 +29,39 @@ export class MergeConflictResolver extends EventEmitter {
         const resolution = await Promise.all(resolutions);
         return new Map(resolution);
     }
+}
+*/
+
+export class MergeConflictResolver {
+  private conflicts: MergeConflict[];
+  private conflictCallback?: (conflict: MergeConflict, resolve: (value: boolean) => void) => void;
+
+  public constructor() {
+      this.conflicts = [];
+  }
+
+  public addConflicts(conflicts: MergeConflict[]) {
+      this.conflicts = conflicts;
+  }
+
+  public onConflict(callback: (conflict: MergeConflict, resolve: (value: boolean) => void) => void) {
+      this.conflictCallback = callback;
+  }
+
+  async resolve(): Promise<Map<string, Cell>> {
+      const resolutions = this.conflicts.map(conflict => 
+          new Promise<[string, Cell]>((resolvePromise) => {
+              if (!this.conflictCallback) {
+                  throw new Error('No conflict resolution callback set');
+              }
+
+              this.conflictCallback(conflict, (value: boolean) => {
+                  resolvePromise([conflict.getCell(), conflict.use(value)]);
+              });
+          })
+      );
+
+      const resolution = await Promise.all(resolutions);
+      return new Map(resolution);
+  }
 }
